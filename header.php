12a6e68525a43f84df0e5983ea3cbb8d4929d944
FernandaTerrazas <85692242+FernandaTerrazas@users.noreply.github.com>
2021-12-01 08:28:48 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>Telcel</title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<img src="logo.png" height="40px" width="150px"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="crud_empleados/empleados.php">Empleados</a></li>
+					<li><a href="crud_clientes/clientes.php">Clientes</a></li>					
+					<li><a href="crud_telefonos/telefonos.php">Telefonos</a></li>
+					<li><a href="crud_paquetes/paquetes.php">Paquetes</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
---
+++ sidebar.php
@@ -0,0 +1,12 @@
+<aside>
+				<div class="widget">
+					<h3>Menú</h3>
+					<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="crud_empleados/empleados.php">Empleados</a></li>
+					<li><a href="crud_clientes/clientes.php">Clientes</a></li>					
+					<li><a href="crud_telefonos/telefonos.php">Telefonos</a></li>
+					<li><a href="crud_paquetes/paquetes.php">Paquetes</a></li>
+					</ul>
+				</div>
+			</aside>
